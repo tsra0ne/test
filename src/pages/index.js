cfe30d6874d64d6ff0dddf51b3047e3cd9cae741
Sravan <tsravangoud164@gmail.com>
2022-11-28 17:20:49 +0530

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Tutorial ⏱️
+            Sravan ⏱️
           </Link>
         </div>
       </div>
